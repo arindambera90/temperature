ac6deb664f30aed25bf339e2dc763abb2067aed7
Arindam Bera <142139111+arindambera90@users.noreply.github.com>
2023-09-03 20:11:57 +0530

    Temperature Converter

---
+++ script.js
@@ -0,0 +1,6 @@
+function convertToCelsius() {
+    var inputCelsius = document.getElementById("inputCelsius").value;
+    var result = (inputCelsius * 9/5) + 32;
+    document.getElementById("result").innerHTML = inputCelsius + "°C is equal to " + result.toFixed(2) + "°F";
+  }
+  
